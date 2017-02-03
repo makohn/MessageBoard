c7bd2f23e7c74c64aa7e5bae470475db751823d4
Junit <schloesserstefan87@googlemail.com>
2017-02-03 19:20:48 +0100

    Message implementation added

--- src/de/htwsaar/remote/remoteModel/impls/MessageImpl.java
+++ src/de/htwsaar/remote/remoteModel/impls/MessageImpl.java
@@ -1,7 +1,65 @@
 package de.htwsaar.remote.remoteModel.impls;
 
-/**
- * Created by stefanschloesser1 on 03.02.17.
- */
-public class MessageImpl {
+import java.io.Serializable;
+import java.rmi.server.UID;
+import java.time.LocalDateTime;
+
+public class MessageImpl implements Serializable {
+
+    private static final long serialVersionUID = -5415774293797687291L;
+
+    private UID id;
+    private String msg;
+    private LocalDateTime time;
+    private String author;
+    private String section;
+
+    public MessageImpl (String msg, LocalDateTime time,String author,String section){
+        this.id = new UID();
+        this.msg = msg;
+        this.author = author;
+        this.section = section;
+        this.time = time;
+
+    }
+
+    public String getMessage(){
+        return msg;
+    }
+
+    public void changeMessage(String msg){
+        this.msg = msg;
+        time = LocalDateTime.now();
+    }
+
+    public LocalDateTime getTime()
+    {
+        return time;
+    }
+
+    public UID getID()
+    {
+        return id;
+    }
+
+    public String getAuthor(){
+        return author;
+    }
+    public String getSection(){
+        return author;
+    }
+    public void setSection(String section){
+        this.section = section;
+    }
+
+    @Override
+    public String toString(){
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("ID : ").append(author).append("\n");
+        sb.append("Abteilung :").append(section).append("\n");
+        sb.append("Time: ").append(time).append("\n");
+        sb.append(msg);
+        return sb.toString();
+    }
 }
