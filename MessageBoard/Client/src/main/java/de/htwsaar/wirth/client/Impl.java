cf524d752c885ee0e9ce9eb8f7ca87b20e9af35d
Achilleus\Shepard <philippschaefer4@gmail.com>
2017-02-28 19:30:48 +0100

    login

--- MessageBoard/Client/src/main/java/de/htwsaar/wirth/client/Impl.java
+++ MessageBoard/Client/src/main/java/de/htwsaar/wirth/client/Impl.java
@@ -3,14 +3,18 @@ package de.htwsaar.wirth.client;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import de.htwsaar.wirth.client.controller.MainViewController;
+import de.htwsaar.wirth.remote.MessageBoard;
 import de.htwsaar.wirth.remote.Notifiable;
+import de.htwsaar.wirth.remote.model.auth.AuthPacket;
+import de.htwsaar.wirth.remote.model.auth.LoginPacket;
 import de.htwsaar.wirth.remote.model.interfaces.Message;
 
 public class Impl extends UnicastRemoteObject implements Notifiable {
 	
-	// auth-Paket-attribut
-	// controller bzw obsListen
-	// parent
+	private AuthPacket auth;
+	private MainViewController gui;
+	private MessageBoard parent;
 
 	protected Impl() throws RemoteException {
 		// TODO Auto-generated constructor stub
@@ -19,9 +23,9 @@ public class Impl extends UnicastRemoteObject implements Notifiable {
 
 	private static final long serialVersionUID = -7940206816319176143L;
 	
-	public void login() {
-		
-		
+	public void login(String username, String password) throws RemoteException {
+		LoginPacket login = new LoginPacket(username, password);
+		auth = parent.registerClient(login, this);		
 	}
 	
 	public void sendMessage() {
